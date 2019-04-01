d2ae7876ef376b2b89c27ede863946d963ab2976
ensar <ensarkarabudak@gmail.com>
2019-04-01 17:24:57 +0300

    Tema-Hata Fix

--- url.py
+++ url.py
@@ -4,23 +4,18 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def index():
-	name="Dostum"
-	age=15
-	return render_template('index.html', name=name, age=age)
-
-@app.route("/register", methods=['POST','GET'])
-def register():
 	if request.method=='POST':
 		url=request.form['url']
 		post_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format('')
 		payload = {'longUrl': url}
 		headers = {'content-type': 'application/json'}
 		cevap = requests.post(post_url, data=json.dumps(payload), headers=headers)
-		return render_template('user_details.html',cevap=cevap)  
+		cevap=cevap.json()
+		return render_template('url.html',cevap=cevap)  
 	else:
-		return render_template('register.html')
+		return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
